25602bb1b8d6c7b7647a2f101239780e195072a6
Ecturing <ecturing@vip.qq.com>
2023-08-13 00:16:39 +0800

    路由树添加请求方法处理，树结构优化

--- ehttp/trie.go
+++ ehttp/trie.go
@@ -1,25 +1,50 @@
 package ehttp
 
 import (
-	"ews/ews_error"
+	"errors"
 )
 
-// 前缀树路由匹配
-
+//初始化
 var (
-	Root = &treeNode{
-		pattern:   "/",
+	//路由树根节点
+	Root = &RootNode{
+		methodNodes: make([]methodNode, 0),
+	}
+	//路由树终止节点
+	ENDNODE = &treeNode{
+		pattern:   "",
 		EndNode:   false,
-		childNode: nil,
+		childNode: make([]treeNode, 0),
 		hander:    nil,
 	}
 )
 
+//路由树根节点结构体
+type RootNode struct {
+	methodNodes []methodNode
+}
+
+//路由树方法节点结构体
+type methodNode struct {
+	method RequestMethod //请求方法
+	node   *treeNode     //路由树节点
+}
+
+// ----------------------------------路由方法节点----------------------------------
+type RequestMethod string
+
+const (
+	GET  RequestMethod = "GET"
+	POST RequestMethod = "POST"
+)
+
+//路由处理函数
 type RouterHandler interface {
 	Register(pattern string, f ServerHTTP)
 	Search(pattern string) (ServerHTTP, error)
 }
 
+//路由树节点
 type treeNode struct {
 	pattern   string     //节点值
 	EndNode   bool       //终止节点
@@ -27,9 +52,38 @@ type treeNode struct {
 	hander    ServerHTTP //函数绑定
 }
 
+// 初始化路由树
+func InitTree() {
+	Root.methodNodes = append(Root.methodNodes, methodNode{
+		method: GET,
+		node:   nil,
+	})
+	Root.methodNodes = append(Root.methodNodes, methodNode{
+		method: POST,
+		node:   nil,
+	})
+}
+
 // 前缀树注册
-func (root *treeNode) Register(pattern string, f ServerHTTP) {
-	currNode := root
+func (root *RootNode) Register(pattern string, f ServerHTTP, method RequestMethod) {
+	var currNode *treeNode
+	switch method {
+	case GET:
+		if root.methodNodes[0].node == nil {
+			root.methodNodes[0].node = ENDNODE
+		}
+		currNode = root.methodNodes[0].node
+
+	case POST:
+		if root.methodNodes[1].node == nil {
+			root.methodNodes[1].node = ENDNODE
+		}
+		currNode = root.methodNodes[1].node
+
+	default:
+		// handle other methods
+		return
+	}
 	for _, v := range pattern {
 		found := false
 		for i := 0; i < len(currNode.childNode); i++ {
@@ -55,10 +109,19 @@ func (root *treeNode) Register(pattern string, f ServerHTTP) {
 }
 
 // 路由树查找
-func (root *treeNode) Search(pattern string) (ServerHTTP, error) {
-	currNode := root
+func (root *RootNode) Search(pattern string, method RequestMethod) (ServerHTTP, error) {
+	var currNode *treeNode
 	var handler ServerHTTP = nil
-	var err *ews_error.EError = nil
+	var err error = nil
+
+	switch method {
+	case GET:
+		currNode = root.methodNodes[0].node
+	case POST:
+		currNode = root.methodNodes[1].node
+	default:
+		// handle other methods
+	}
 	for _, v := range pattern {
 		found := false
 		for i := 0; i < len(currNode.childNode); i++ {
@@ -69,15 +132,11 @@ func (root *treeNode) Search(pattern string) (ServerHTTP, error) {
 			}
 		}
 		if !found {
-			err = &ews_error.EError{
-				Msg: "routerERR:invalid router can't find it",
-			}
+			err = errors.New("404,routerERR:invalid router can't find it")
 		}
 	}
 	if !currNode.EndNode {
-		err = &ews_error.EError{
-			Msg: "routerERR:invalid router!",
-		}
+		err = errors.New("404,routerERR:invalid router")
 	}
 	return handler, err
 }
