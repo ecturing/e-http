b6581d2f8eaca5285b3ad5d3f7740aa0db5441aa
ecturing <ecturing@vip.qq.com>
2024-01-08 14:47:06 +0800

    更改main函数

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 
 func main() {
 	r := ehttp.NewRouter()
-	ehttp.Server(r,"/server", server,ehttp.GET)
+	ehttp.Server(r, "/server", server, ehttp.POST)
 	ehttp.Confirm(":8080")
 }
 
-func server(rq *ehttp.Request, rp *ehttp.Response) {
-	rp.DataFrom = "reponse"
+func server(rq *ehttp.E_Request, rp *ehttp.E_Response) {
+	rp.Headers["Content-Type"] = "text/plain"
+	rp.DataFrom = rq.Proto
 }
