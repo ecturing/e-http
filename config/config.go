107eee390c1602d577873e501d5752cc976cabb5
ecturing <ecturing@vip.qq.com>
2024-01-16 13:32:34 +0800

    yml配置文件

---
+++ config/config.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+var (
+	Config *yaml.Decoder
+)
+
+func init() {
+	// 初始化yaml配置文件器
+	file, err := os.Open("config/config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	Config = yaml.NewDecoder(file)
+}
