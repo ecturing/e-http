945fcf55423cf910bb8d634eeddbf5dc67f2deb1
ecturing <ecturing@vip.qq.com>
2024-03-03 18:55:34 +0800

    Refactor socket handling and add context support

--- ehttp/socket.go
+++ ehttp/socket.go
@@ -3,9 +3,18 @@ package ehttp
 import (
 	"bufio"
 	"bytes"
-	"net"
+	"context"
+	e_context "ews/context"
 	"ews/log"
+	"net"
+	"time"
 )
+
+const (
+	DEADLINETIME = 10 * time.Second
+)
+
+type ConnClose bool
 
 type Event struct {
 	// Conn   *net.TCPConn
@@ -13,8 +22,13 @@ type Event struct {
 	Writer *bytes.Buffer
 }
 
+type ServerCtx struct {
+	conn      *net.TCPConn
+	serverArg ServerArg
+}
+
 // 定义socket绑定点，并将socket交给Linux epoll管理，增强并发率
-func InitSocket(address string) error {
+func (s *ServerArg) ServerHTTP(address string) error {
 	log.Logger.Info().Msgf("Starting TCP server on %s", address)
 	tcpAddr, err := net.ResolveTCPAddr("tcp", address)
 	if err != nil {
@@ -26,11 +40,11 @@ func InitSocket(address string) error {
 	}
 	defer listener.Close()
 
-	listenerHandler(listener)
+	listenerHandler(s, listener)
 	return nil
 }
 
-func listenerHandler(listener *net.TCPListener) {
+func listenerHandler(s *ServerArg, listener *net.TCPListener) {
 	log.Logger.Info().Msg("Tcp listener start")
 	for {
 		conn, err := listener.AcceptTCP()
@@ -39,38 +53,50 @@ func listenerHandler(listener *net.TCPListener) {
 			continue
 		}
 		log.Logger.Info().Msgf("Accepted connection from %s", conn.RemoteAddr().String())
-		go connectionReadBuf(conn)
-		go connectWriteBuf(conn)
+		// conn.SetDeadline(time.Now().Add(DEADLINETIME))
+		ctx := context.WithValue(e_context.RootCtx, "conn", &ServerCtx{conn, *s})
+		// todo
+		// 未来考虑采用IO多路复用技术
+		go connectionReadBuf(ctx)
+		go connectWriteBuf(ctx)
 	}
 }
 
 // 读取链接缓冲区准备读取的事件并发送到管道
-func connectionReadBuf(c *net.TCPConn) {
+func connectionReadBuf(ctx context.Context) {
+	serverCtx := ctx.Value("conn").(*ServerCtx)
+	c := serverCtx.conn
+	r := serverCtx.serverArg.Router
 	readevent := bufio.NewReader(c)
-	var event = &Event{
-		// Conn:   c,
-		Reader: readevent,
-		Writer: nil,
+	for {
+		ok := ReadRequest(r, readevent)
+		if ok {
+			CloseQueen <- struct{}{}
+			break
+		}
 	}
-	ReadQueen <- event
 }
 
 // 写入链接的缓冲区
-func connectWriteBuf(c *net.TCPConn) {
-	select {
-	case s := <-WriteQueen:
-		// do
-		w := bufio.NewWriter(c)
-		w.Write(s.Writer.Bytes())
-		w.Flush()
-		c.Close()
-		log.Logger.Info().Msgf("%s TCP leave the server", c.RemoteAddr().String())
-	case s := <-ErrorSingal:
-		// do
-		w := bufio.NewWriter(c)
-		w.Write(s.Writer.Bytes())
-		w.Flush()
-		c.Close()
-		log.Logger.Info().Msgf("%s TCP leave the server", c.RemoteAddr().String())
+func connectWriteBuf(ctx context.Context) {
+	sctx := ctx.Value("conn").(*ServerCtx)
+	for {
+		select {
+		case s := <-WriteQueen:
+			// do
+			w := bufio.NewWriter(sctx.conn)
+			w.Write(s.Writer.Bytes())
+			w.Flush()
+			log.Logger.Info().Msgf("%s TCP the server return msg", sctx.conn.RemoteAddr().String())
+		case s := <-ErrorSingal:
+			// do
+			w := bufio.NewWriter(sctx.conn)
+			w.Write(s.Writer.Bytes())
+			w.Flush()
+			sctx.conn.Close()
+			log.Logger.Info().Msgf("%s TCP leave the server", sctx.conn.RemoteAddr().String())
+		case <-CloseQueen:
+			return
+		}
 	}
 }
